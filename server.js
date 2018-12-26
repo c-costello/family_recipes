f9166a03ab3f8c33c09f879943c7e36953b87c98
Clarice <claricostello@gmail.com>
2018-12-26 14:22:02 -0800

    set up save function for new recipe

--- server.js
+++ server.js
@@ -43,9 +43,30 @@ app.listen(PORT, () => {
 });
 //function calls
 app.get('/', getHomePage);
+app.get('/new', getNewRecipe);
+app.post('/new', saveNewRecipe);
 
 
 //index functions
 function getHomePage(req, res){
   res.render('pages/index.ejs');
+}
+
+
+
+//new recipe functions
+//render the form to save a new recipe
+function getNewRecipe(req, res){
+  res.render('pages/recipes/new.ejs')
+}
+//saves the new recipe when submit button is selected 
+function saveNewRecipe(req, res){
+  console.log('save new recipe is firing');
+  console.log(req.body);
+  let { title, description, created_by, source } = req.body;
+  let SQL = `INSERT INTO recipes (title, description, created_by, source) VALUES($1,$2,$3,$4) RETURNING id;`;
+  let values = [title, description, created_by, source];
+  client
+    .query(SQL, values)
+    .then(res.redirect('/new'))
 }
