9dbde821f822e268c7caad90ab41fa76ab177c10
Clarice <claricostello@gmail.com>
2018-12-26 13:28:13 -0800

    set up basic schema file

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS instructions;
+DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE recipes(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  created_by VARCHAR(255),
+  source VARCHAR (255),
+
+);
+CREATE TABLE instructions(
+  id SERIAL PRIMARY KEY,
+  step_number NUMERIC(0,4),
+  instruction TEXT,
+  recipe_id INTEGER NOT NULL,
+  FOREIGN KEY (recipe_id) REFERENCES recipes (id)
+);
+
+CREATE TABLE ingredients(
+  id SERIAL PRIMARY KEY,
+  ingredient_number NUMERIC(0,4),
+  ingredients TEXT,
+  recipe_id INTEGER NOT NULL,
+  FOREIGN KEY (recipe_id) REFERENCES recipes (id)
+);
+
